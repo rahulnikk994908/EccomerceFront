069f9d41c52c546d5a0abbe6f14ac13502bbbf17
Envy365 <kijie5999@gmail.com>
2023-10-14 08:47:06 +0530

    revert changes

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL ='http://localhost:5455'
+export const API_BASE_URL ="https://eccomercebackend-production.up.railway.app"
 
 const jwt = localStorage.getItem("jwt")
 
